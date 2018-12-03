c8d146fd696ae85cf5d7fb77da61a0f252342e26
_xiaotian <952822399@qq.com>
2018-12-03 22:36:00 +0800

    雀语

--- antd-course/src/model/puzzlecards.js
+++ antd-course/src/model/puzzlecards.js
@@ -1,14 +1,40 @@
+import request from '../util/request';  // request 是 demo 项目脚手架中提供的一个做 http 请求的方法，是对于 fetch 的封装，返回 Promise
+
+const delay = (millisecond) => {
+  return new Promise((resolve) => {
+    setTimeout(resolve, millisecond);
+  });
+};
+
 export default {
   namespace: 'puzzlecards',
-  state: [
-    { id: 1,
-      setup: 'Did you hear about the two silk worms in a race?',
-      punchline: 'It ended in a tie',
-    },
-    {
-      id: 2,
-      setup: 'What happens to a frog\'s car when it breaks down?',
-      punchline: 'It gets toad away',
-    },
-  ],
+  state: {
+    data: [],
+    counter: 0,
+      },
+  effects: {
+    *queryInitCards(_, sagaEffects) {
+      const { call, put } = sagaEffects;
+      const endPointURI = 'https://08ad1pao69.execute-api.us-east-1.amazonaws.com/dev/random_joke';
+
+      const puzzle = yield call(request, endPointURI);
+      yield put({ type: 'addNewCard', payload: puzzle });
+
+      yield call(delay, 3000);
+
+      const puzzle2 = yield call(request, endPointURI);
+      yield put({ type: 'addNewCard', payload: puzzle2 });
+    }
+  },
+  reducers: {
+    addNewCard(state, { payload: newCard }) {
+      const nextCounter = state.counter + 1;
+      const newCardWithId = { ...newCard, id: nextCounter };
+      const nextData = state.data.concat(newCardWithId);
+      return {
+        data: nextData,
+        counter: nextCounter,
+      };
+    }
+  },
 };

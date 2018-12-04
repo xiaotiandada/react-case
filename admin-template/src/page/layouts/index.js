0487f87aa98368dd1c21b3ff5a30d047fffdd82d
_xiaotian <952822399@qq.com>
2018-12-04 22:50:21 +0800

    admin-template

---
+++ admin-template/src/page/layouts/index.js
@@ -0,0 +1,12 @@
+import React, { Component } from 'react';
+class BasicLayout extends Component {
+  render(){
+    return (
+      <div>
+          Learn React
+      </div>
+    )
+  }
+}
+
+export default BasicLayout
